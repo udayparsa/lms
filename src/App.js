e8c8a6b35193d5d91da14e78db6398b755f28bf4
udayparsa <udayparsa69@gmail.com>
2024-09-29 18:04:28 +0530

    31172

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Register from './ui/register';
 import Login from './ui/login';
 import Home from './ui/home';
+import Main from './ui/main';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Route path='/' element={<Home />} />
       <Route path='/login' element={<Login/>} />
       <Route path='/register' element={<Register/>} />
+      <Route path='/main' element={<Main/>}/>
      </Routes>
      </BrowserRouter>
     </div>
---
+++ src/ui/main.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function main() {
+  return (
+    <div>
+        <h1>This is main page</h1>
+    </div>
+  )
+}
